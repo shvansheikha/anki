f913179dcb977f54f7a70efe147bc7437d7c85da
shvanasheikha <shvansheikha@gmail.com>
2022-03-25 10:36:53 +0430

    second command

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Jackwestin\AnkiSandbox\app\Console\AnkiCommand;
+use Jackwestin\AnkiSandbox\app\Console\AnkiCommandTwo;
 use Symfony\Component\Console\Application;
 
 require __DIR__ . '/vendor/autoload.php';
@@ -8,6 +9,7 @@ require __DIR__ . '/vendor/autoload.php';
 $app = new Application();
 
 $app->add(new AnkiCommand());
+$app->add(new AnkiCommandTwo());
 
 $app->run();
 ?>
